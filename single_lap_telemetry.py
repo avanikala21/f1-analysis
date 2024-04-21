23258f01c6d172317ccab6f9c7bf2507834cc840
Avani Kala <kala.a@northeastern.edu>
2024-04-21 16:57:05 -0500

    fix plot labelling

--- single_lap_telemetry.py
+++ single_lap_telemetry.py
@@ -11,21 +11,23 @@ fastf1.plotting.setup_mpl()
 session = fastf1.get_session(2024, 'China', 'S')
 session.load(telemetry=True, laps=True, weather=False)
 
-
 #%%
 circuit_info = session.get_circuit_info()
+
+
 #%%
 def driver_lap_telemetry(lap_number, driver_names):
+    v_min, v_max = 0, 0
     for name in driver_names:
         telemetry = session.laps.pick_laps(lap_number).pick_driver(name).get_car_data().add_distance()
         driver_speed = telemetry['Speed']
         driver_distance = telemetry['Distance']
-        plt.plot(driver_distance, driver_speed)
+        plt.plot(driver_distance, driver_speed, label=name)
         v_min = driver_speed.min()
         v_max = driver_speed.max()
-    plt.vlines(x=circuit_info.corners['Distance'], ymin=v_min-20, ymax=v_max+20, linestyles='dotted', colors='grey')
+    plt.vlines(x=circuit_info.corners['Distance'], ymin=v_min - 20, ymax=v_max + 20, linestyles='dotted', colors='grey')
     mplcursors.cursor(hover=True, highlight=True)
-    plt.legend(driver_names)
+    plt.legend()
     plt.title(f'Lap {lap_number} Telemetry')
     plt.xlabel('Distance')
     plt.ylabel('Speed (km/h)')
